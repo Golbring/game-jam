b1b45aee09d4f25fddd70980b1f8edade6abb306
Golbring <francesco.kramer@hotmail.com>
2025-01-30 20:22:00 +0100

    Some more fixes

--- Assets/Scripts/SceneHandler.cs
+++ Assets/Scripts/SceneHandler.cs
@@ -73,7 +73,7 @@ public class SceneHandler : MonoBehaviour
         {
             SceneManager.LoadScene(currentSceneIdx + 1);
         }
-        else { return ; }
+        else { LoadMenu(); }
     }
 }
 
